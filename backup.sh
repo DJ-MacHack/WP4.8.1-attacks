f618f8d995130fba569f399f41f6412ebfc9a783
DJ MacHack <mail@hendrik-haas.de>
2019-01-29 19:40:35 +0100

    sh sec

---
+++ backup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+tar -cJpf /media/backup/wp-backup-`date +%d-%m-%Y`.tar.xz /var/www/wp
--- secure.sh
+++ secure.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#install wp-cli
+sudo wget -q https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/bin/wp
+sudo chown user /usr/bin/wp
+sudo chmod 755 /usr/bin/wp
+
 #array of plugin to install
 #Nr: 1 2 (3 is in 1) (4 in 1 or use script) (5? I do not wanna share my passwords) 6 7 (8 a automated Lets encrypt is possible but too much) 9 (10 in 1) 11 12 15 (18 in 1) 21
 WPPLUGINS=( better-wp-security miniorange-2-factor-authentication bulletproof-security askapache-password-protect force-strong-passwords wordfence wp-dbmanager wp-security-audit-log all-in-one-wp-security-and-firewall )
@@ -22,7 +27,9 @@ sudo find $WPPATH -type d -exec chmod 755 {} +
 mv $WPPATH/wp-login.php $WPPATH/my_secure_login.php
 mv $WPPATH/wp-admin $WPPATH/my_secure_admin
 #add cron daily backup Nr 13
-#TODO
+chmod +X $WPPATH/backup.sh
+echo "0 3 * * * root" + $WPPATH + "/backup.sh" >> /etc/crontab
+echo "" >> /etc/crontab
 #move wp-config.php Nr 16
 mv $WPPATH/wp-config.php $WPPATH/../wp-config.php
 #Nr 17
